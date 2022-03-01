fe80054653713b06cd84be5ad2f80b30a102f6dc
KyleBrogdon <brogdonk@oregonstate.edu>
2022-02-28 21:13:22 -0600

    working on pokemon page still

--- app.py
+++ app.py
@@ -10,23 +10,50 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/pokemon.html')
+@app.route('/pokemonSearchResults')
+def pokemonSearch(pokedexNumber, pokemonSearchName):
+    db_connection = connect_to_database()
+    if pokedexNumber is None and pokemonSearchName is None:  # if both none
+        return "No results found"
+    elif pokedexNumber is None or pokemonSearchName is None:  # if one of them is none
+        if pokedexNumber is None:
+            query = "SELECT * FROM Pokemon WHERE pokemonSearchName == pokemonName"
+            result = execute_query(db_connection, query).fetchall()
+            if result is None:  # if still none
+                return "No results found"
+            render_template ("pokemonSearchResults.html", rows = result)
+        else:
+            query = "SELECT * FROM Pokemon WHERE pokedexNumber == pokemonId and pokemonSearchName == pokemonName"
+            result = execute_query(db_connection, query).fetchall()
+            if result is None:  # if still none
+                return "No results found"
+            render_template ("pokemonSearchResults.html", rows = pokemonSearchName)
+
+@app.route('/pokemon.html', methods = ['POST', 'GET'])
 def pokemon():
     db_connection = connect_to_database()
-    query = "SELECT * FROM Pokemon"
-    result = execute_query(db_connection, query).fetchall()
+    regions = 'SELECT * FROM Regions'  #populate dropdowns later
+    types = 'SELECT * FROM Types'   # populate dropdowns later
+    if request.method == 'GET':
+        if request.form.get('searchButton'):  # if search button gets pressed, treat as get
+            pokedexNumber = request.form['Pokedex Search']
+            pokemonSearchName = request.form['Name Search']
+            return redirect(url_for('pokemonSearchResults', pokedexNumber, pokemonSearchName))  # redirect and pass values to search result page
+        else:  # else it's just the initial render, add regions and types here with jinja later
+            query = "SELECT * FROM Pokemon"
+            result = execute_query(db_connection, query).fetchall()
+            return render_template("pokemon.html", rows = result)
+    
+    if request.method == 'POST':  # handle add new pokemon
+        pass
 
     # query to add new pokemon
-        # query to display gender dropdown
         # query to display region dropdown
         # query to display type 1
         # query to display type 2
-    
-    # query to search for a pokemon by pokedex number
-    # query to search for a pokemon by name
 
     # query to delete pokemon by pokedex number
-    return render_template("pokemon.html", rows = result)
+
 
 @app.route('/pokemontypes.html')
 def pokemontypes():
@@ -70,4 +97,4 @@ def types():
 if __name__ == "__main__":
 
     #Start the app on port 3000, it will be different once hosted
-    app.run(port=31278, debug=False)
+    app.run(port=31479, debug=False)
